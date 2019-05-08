3920a54775ff43f1fddc2ebbb34d888d06745dec
362368904@qq.com <362368904@qq.com>
2019-05-08 17:55:13 +0800

    第一次提交

---
+++ emergency/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <ChengJi/>
+    
+      <div>
+        <DanXuanKuang/>
+        <DaoJiShi></DaoJiShi>
+      </div>
+      <div>
+         <DaoJiShi2 @time-end="message = '倒计时结束'" :endTime='endTime'></DaoJiShi2>
+         <p>{{message}}</p> 
+      </div>
+
+  </div>
+</template>
+
+<script>
+
+
+import DaoJiShi2 from './components/DaoJiShi2'
+import DanXuanKuang from './components/DanXuanKuang'
+
+export default {
+  name: 'App',
+  components: {
+
+
+    DanXuanKuang,
+
+    DaoJiShi2
+  },
+  data(){
+    return{
+      message:'正在倒计时',
+      endTime : '2019-07-08 10:06:00'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ emergency/src/components/DanXuanKuang.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="Emergency">
+        <div>应急预案编号</div>
+        <form v-on:submit="EmergencyAction">    
+            <select>
+                <option value="1">广域网单边链路中断应急预案</option>
+                <option value="2">广域网单边链路丢包应急预案</option>
+            </select>
+            
+            <input type="time"  value="13:55:59" >
+
+            <input type="submit" value="提交">
+
+
+        </form>       
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'EmergencyAction',
+        data:function(){
+            return{
+                drill_num:""
+            }
+        },
+        methods:{
+        EmergencyAction:function(){
+
+            alert("提交成功");
+        }
+        }
+    }
+</script>
