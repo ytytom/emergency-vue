131c48303f2615d6c933f0a87d788eadc1b9c420
362368904@qq.com <362368904@qq.com>
2019-05-10 19:01:22 +0800

    第三次提交

--- emergency_2/src/components/Emergency.vue
+++ emergency_2/src/components/Emergency.vue
@@ -1,43 +1,45 @@
 <template>
   <div id="Emergency">
-    <form v-on:submit="EmergencyAction">
-      <el-select v-modle="Drill_num" placeholder="请选择预案编号">
-        <el-option value="shanghai">次生产区域第三方IpSec接入网关第一台YZXIPSECFW04故障应急预案</el-option>
-        <el-option value="beijing">省市体彩中心数据端二次认证服务失效应急预案</el-option>
-      </el-select>
-      <br>
-      <br>
-      <br>
-      <DateTimePicker type="datetime-local" name="time" v-model='date'></DateTimePicker>
-      <br>
-      <el-button type="primary" @click="onSubmit">立即创建</el-button>
-      <el-button>取消</el-button>
-    </form>
+    <div>应急预案编号</div>
+    <Test v-on:formSubmit="Getformitem"></Test>
+    <DaoJiShi @time-end="message='倒计时结束'" :endTime='endTime'></DaoJiShi>
+    <P v-if="endTime">{{message}}</P>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
 import DateTimePicker from '@/components/DateTimePicker'
+import Test from '@/components/Test'
+import DaoJiShi from '@/components/DaoJiShi'
 export default {
   name: 'Emergency',
+  components: {
+    DateTimePicker,
+    Test,
+    DaoJiShi
+  },
   data(){
     return{
-      date,
-      Drill_num
+      message: '正在倒计时',
+      endTime: ''
     }
-  },
-  components: {
-    DateTimePicker
   },
   methods: {
-    onSubmit() {
-      alert('submit!');
+    EmergencyAction: function(){
+      alert("hello world!")
+    },
+    Getformitem:function(text){
+      console.log(111111111111111111111111)
+      // alert(text)
+      this.endTime = text
     }
   }
+
 }
 </script>
 
 <style>
+
 
 </style>
--- emergency_2/src/components/Test.vue
+++ emergency_2/src/components/Test.vue
@@ -1,140 +1,142 @@
 <template>
-  <div id='Test' class="block">
-    <span class="demonstration">预案选择</span>
-    <el-cascader
-      :options="options"
-      v-model="selectedOptions"
-      @change="handleChange">
-    </el-cascader>
+  <div id='Test' class='test'>
+    <el-form ref="form" :model="form" label-width="80px" :rules="rules">
+      <el-form-item label="test" prop="test">
+        <el-input v-model="form.test"></el-input>
+      </el-form-item>
+      <el-form-item label="应急预案" prop="drill">
+        <el-cascader
+        :options="form.options"
+        placeholder="请选择应急预案"
+        v-model="selectedOptions"
+        @change="handleChange">
+        </el-cascader>
+      </el-form-item>
+      <el-form-item label="触发时间" prop="date1">
+        <el-date-picker
+          v-model="form.date1"
+          type="datetime"
+          placeholder="选择日期时间"
+          align="right"
+          :picker-options="pickerOptions">
+        </el-date-picker>
+      </el-form-item>
+      <el-button type="primary" @click="onSubmit('form')">立即创建</el-button>
+      <el-button type="reset" @click="resetForm('form')">取消</el-button>
+    </el-form>
   </div>
 </template>
+
 <script>
 /* eslint-disable */
   export default {
     name: 'Test',
     data() {
       return {
-        options: [{
-          label: '广域网单边链路中断应急预案',
-          children: [{
-            value: '001',
-            label: '主节点',
-          },{
-            value: '002',
-            label: '备节点',
+        rules:{
+          test:[{ required: true, message: '请输入活动名称', trigger: 'blur' }],
+          date1:[{ type:'date',required: true,message:'请选择日期',trigger:'change'}]
+        },
+        form: {
+          test:'',
+          date1: '',
+          options: [{value: 'WL001',label: '广域网单边链路中断应急预案',
+                    children: [{value: '001',label: '主节点',},
+                              {value: '002',label: '备节点',}]
+                    }, 
+                    {value: 'WL002',label: '广域网单边链路丢包应急预案',
+                    children: [{value: '003',label: '主节点',}, 
+                                {value: '004',label: '备节点',}]},
+                    {value:'WL003',label:'次生产互联网区域YZXCRT03路由器故障应急预案'},
+                    {value: 'ziyuan',label: '资源',
+                    children: [{value: 'axure',label: 'Axure Components'},
+                               {value: 'sketch',label: 'Sketch Templates'},
+                                {value: 'jiaohu',label: '组件交互文档' }]
+                    }],
+        },
+        pickerOptions: {
+          shortcuts: [{
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '明天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() + 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '后天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() + 3600 * 1000 * 24 * 2);
+              picker.$emit('pick', date);
+            }
           }]
-        }, {
-          label: '广域网单边链路丢包应急预案',
-          children: [{
-            value: '003',
-            label: '主节点',
-          }, {
-            label: '备节点',
-          }, {
-            value: 'data',
-            label: 'Data',
-            children: [{
-              value: 'table',
-              label: 'Table 表格'
-            }, {
-              value: 'tag',
-              label: 'Tag 标签'
-            }, {
-              value: 'progress',
-              label: 'Progress 进度条'
-            }, {
-              value: 'tree',
-              label: 'Tree 树形控件'
-            }, {
-              value: 'pagination',
-              label: 'Pagination 分页'
-            }, {
-              value: 'badge',
-              label: 'Badge 标记'
-            }]
-          }, {
-            value: 'notice',
-            label: 'Notice',
-            children: [{
-              value: 'alert',
-              label: 'Alert 警告'
-            }, {
-              value: 'loading',
-              label: 'Loading 加载'
-            }, {
-              value: 'message',
-              label: 'Message 消息提示'
-            }, {
-              value: 'message-box',
-              label: 'MessageBox 弹框'
-            }, {
-              value: 'notification',
-              label: 'Notification 通知'
-            }]
-          }, {
-            value: 'navigation',
-            label: 'Navigation',
-            children: [{
-              value: 'menu',
-              label: 'NavMenu 导航菜单'
-            }, {
-              value: 'tabs',
-              label: 'Tabs 标签页'
-            }, {
-              value: 'breadcrumb',
-              label: 'Breadcrumb 面包屑'
-            }, {
-              value: 'dropdown',
-              label: 'Dropdown 下拉菜单'
-            }, {
-              value: 'steps',
-              label: 'Steps 步骤条'
-            }]
-          }, {
-            value: 'others',
-            label: 'Others',
-            children: [{
-              value: 'dialog',
-              label: 'Dialog 对话框'
-            }, {
-              value: 'tooltip',
-              label: 'Tooltip 文字提示'
-            }, {
-              value: 'popover',
-              label: 'Popover 弹出框'
-            }, {
-              value: 'card',
-              label: 'Card 卡片'
-            }, {
-              value: 'carousel',
-              label: 'Carousel 走马灯'
-            }, {
-              value: 'collapse',
-              label: 'Collapse 折叠面板'
-            }]
-          }]
-        }, {
-          value: 'ziyuan',
-          label: '资源',
-          children: [{
-            value: 'axure',
-            label: 'Axure Components'
-          }, {
-            value: 'sketch',
-            label: 'Sketch Templates'
-          }, {
-            value: 'jiaohu',
-            label: '组件交互文档'
-          }]
-        }],
+        },
         selectedOptions: [],
-        selectedOptions2: []
+        
       };
     },
+
+    // 上面全是数据
     methods: {
       handleChange(value) {
-        console.log(value);
+        if(value[1]){
+          console.log(value[1]);
+        }
+        else{
+          console.log(value[0]);
+        }
+      },
+      onSubmit(form){
+        this.$refs[form].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+        // tep = this.form.date1+this.form.date2;
+        this.$emit("formSubmit",this.form.date1)
+        // alert(this.form.date1)
+        // console.log(this.form.options)
+      },
+      ResetOptions(formName){
+        this.$refs[formName].resetFields();
       }
     }
   };
 </script>
+
+
+<style>
+.test{
+  border: 1px solid #DCDFE6;
+    width: 350px;
+    margin: 180px auto;
+    padding: 35px 35px 15px 35px;
+    border-radius: 5px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    box-shadow: 0 0 250px #909399;
+}
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+div {
+  width: 300px;
+  height: 300px;
+  margin: 30px auto;
+  border: 1px solid red;
+
+}*/
+</style>
